3d69d0da26bc9929f232f359d22ccfc1eb015aa5
AndyLucaas <lucasbekibo@gmail.com>
2026-08-14 15:44:33 +0300

    refactor: uncomment bootstrap function and its contents in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,15 @@ import { App } from './app/App';
 import { initKeycloak, scheduleTokenRefresh } from './features/auth/keycloak';
 import './styles/index.css';
 
-// async function bootstrap() {
-//   await initKeycloak();
-//   scheduleTokenRefresh();
+async function bootstrap() {
+  await initKeycloak();
+  scheduleTokenRefresh();
 
-//   createRoot(document.getElementById('root')!).render(
-//     <StrictMode>
-//       <App />
-//     </StrictMode>,
-//   );
-// }
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
 
-// bootstrap();
+bootstrap();
