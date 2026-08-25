f2c9d96291722a6be6316bcbdd31cf604da2433a
AndyLucaas <lucasbekibo@gmail.com>
2026-08-25 13:47:46 +0300

    feat: add Terraform validate step and fix security group ID assignment in EC2 instance module

--- infrastructure/ec2_terraform/main.tf
+++ infrastructure/ec2_terraform/main.tf
@@ -69,7 +69,7 @@ module "ec2_instance" {
   ami                         = "ami-05bfa4a7765f38076"
   key_name                    = "sonar_srv_key"
   monitoring                  = true
-  vpc_security_group_ids      = [module.sg.security_group_id]
+  vpc_security_group_ids      = module.sg.security_group_id
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   availability_zone           = data.aws_availability_zones.azs.names[0]
